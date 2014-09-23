f6df712bdb127847519b3c3477ac0ffab1c9cca6
Caio Cabral <caio.cabral@gmail.com>
2014-09-23 14:36:46 -0300

    Inclusão de projeto de teste unitário

---
+++ Teste/DAL/TestBaseEntity.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DAL.Repository;
+
+namespace Teste.DAL
+{
+    [TestClass]
+    public class TestBaseEntity
+    {
+        [TestMethod]
+        public void TestPopularBaseDados()
+        {
+            new BaseRepository().PopularBaseDados();
+        }
+
+        [TestMethod]
+        public void TestRecriarBaseDados()
+        {
+            new BaseRepository().RecriarBaseDados();
+        }
+    }
+}
